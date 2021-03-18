1b488fe3f564c4b6957a3ff995ca9f7c096ed48d
Aditya Prakash Kushwaha <48464775+Aditya8014@users.noreply.github.com>
2021-03-19 01:17:45 +0545

    set the click listener for the button

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.fragments;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String STATE_FRAGMENT = "state_of_fragment";
+    private Button mButton;
+    private boolean isFragmentDisplayed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = findViewById(R.id.open_button);
+
+        if (savedInstanceState != null) {
+            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT);
+
+            if (isFragmentDisplayed) {
+                // if the fragment is displayed, change button to close
+                mButton.setText(R.string.close);
+            }
+        }
+
+        // set the click listener for the button
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isFragmentDisplayed) {
+                    displayFragment();
+                } else {
+                    closeFragment();
+                }
+            }
+        });
     }
 }
